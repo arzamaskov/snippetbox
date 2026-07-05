2e182a2d2386f7aef895ae40fd139ca3d8b6ea78
Andrey Arzamaskov <arzamaskov@gmail.com>
2026-07-05 16:52:52 +0400

    refactor(web): centralize HTTP error handling

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -13,24 +13,11 @@ type application struct {
 }
 
 func main() {
-	addr := flag.String(
-		"addr",
-		":4000",
-		"HTTP network address",
-	)
+	addr := flag.String("addr", ":4000", "HTTP network address")
 	flag.Parse()
 
-	infoLog := log.New(
-		os.Stdout,
-		"INFO\t",
-		log.Ldate|log.Ltime,
-	)
-
-	errorLog := log.New(
-		os.Stderr,
-		"ERROR\t",
-		log.Ldate|log.Ltime|log.Lshortfile,
-	)
+	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
+	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
 
 	app := &application{
 		errorLog: errorLog,
@@ -40,10 +27,7 @@ func main() {
 	mux := http.NewServeMux()
 	fileServer := http.FileServer(http.Dir("ui/static/"))
 
-	mux.Handle(
-		"/static/",
-		http.StripPrefix("/static", fileServer),
-	)
+	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
 	mux.HandleFunc("/", app.home)
 	mux.HandleFunc("/snippet/view", app.snippetView)
 	mux.HandleFunc("/snippet/create", app.snippetCreate)
@@ -53,6 +37,7 @@ func main() {
 		ErrorLog: errorLog,
 		Handler:  mux,
 	}
+
 	infoLog.Printf("Starting Server on %s", *addr)
 	err := srv.ListenAndServe()
 	errorLog.Fatal(err)
