aab7e724b96b9383124120bdf6eea3878a62786d
Andrey Arzamaskov <arzamaskov@gislavedtires.ru>
2026-07-14 17:22:50 +0400

    refactor(web): improve template rendering safety and flexibility

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"database/sql"
 	"flag"
-	"fmt"
 	"html/template"
 	"log"
 	"net/http"
@@ -71,19 +70,4 @@ func openDB(dsn string) (*sql.DB, error) {
 	}
 
 	return db, nil
-}
-
-func (app *application) render(w http.ResponseWriter, status int, page string, data *templateData) {
-	ts, ok := app.templateCache[page]
-	if !ok {
-		err := fmt.Errorf("the template %s does not exist", page)
-		app.serverError(w, err)
-		return
-	}
-
-	w.WriteHeader(status)
-	err := ts.ExecuteTemplate(w, "base", data)
-	if err != nil {
-		app.serverError(w, err)
-	}
 }
